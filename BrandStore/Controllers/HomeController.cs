5e1802ab27cad204caa21f3d2a6db2f0fbf831b1
SuleAktas <suleaktas@live.nl>
2021-12-06 15:27:24 +0300

    Marka 'filtrelemesine gore Index sayfasi duzenlendi.Kaydedilen kullanicilarin rolleri atandi.

--- BrandStore/Controllers/HomeController.cs
+++ BrandStore/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
-﻿using BrandStore.Models;
+﻿using BrandStore.Areas.Identity.Data;
+using BrandStore.Data;
+using BrandStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BrandStore.Controllers
@@ -12,15 +20,50 @@ namespace BrandStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext context, IHttpContextAccessor httpContextAccessor, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateBrand()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateBrand([Bind("Id,Name,Description,Active")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.ApplicationUserId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                brand.Active = true;
+                var user = await _userManager.FindByIdAsync(brand.ApplicationUserId);
+                await _userManager.AddToRoleAsync(user, "Admin");
+                _context.Add(brand);
+                await _context.SaveChangesAsync();
+
+            }
+            return View(brand);
+        }
+
+
+        public async Task<IActionResult> BrandIndex(string brand)
+        {
+            List<Product> _urunler = await _context.Products.Where(b=>b.Brand.Name==brand).OrderByDescending(x => x.CreateDate).Take(10).ToListAsync();
+            return View(_urunler);
         }
 
         public IActionResult Privacy()
